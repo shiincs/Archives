642885ecdbee1350bf2cf96eb71a57d2144a6fa0
Shin Changseon <skywing27@gmail.com>
2019-02-16 00:16:42 +0900

    TIL: advance lesson 26-27

--- Redux Study with FDS/redux-todo/src/actions/index.js
+++ Redux Study with FDS/redux-todo/src/actions/index.js
@@ -1,3 +1,5 @@
+import { normalize } from 'normalizr';
+import * as schema from './schema';
 import { getIsFetching } from '../reducers';
 import * as api from '../api';
 
@@ -12,10 +14,14 @@ export const fetchTodos = filter => (dispatch, getState) => {
 
   return api.fetchTodos(filter).then(
     response => {
+      console.log(
+        'normalized response',
+        normalize(response, schema.arrayOfTodos)
+      );
       dispatch({
         type: 'FETCH_TODOS_SUCCESS',
         filter,
-        response,
+        response: normalize(response, schema.arrayOfTodos),
       });
     },
     error => {
@@ -30,13 +36,17 @@ export const fetchTodos = filter => (dispatch, getState) => {
 
 export const addTodo = text => dispatch =>
   api.addTodo(text).then(response => {
+    console.log('normalized response', normalize(response, schema.todo));
     dispatch({
       type: 'ADD_TODO_SUCCESS',
-      response,
+      response: normalize(response, schema.todo),
     });
   });
 
-export const toggleTodo = id => ({
-  type: 'TOGGLE_TODO',
-  id,
-});
+export const toggleTodo = id => dispatch =>
+  api.toggleTodo(id).then(response => {
+    dispatch({
+      type: 'TOGGLE_TODO_SUCCESS',
+      response: normalize(response, schema.todo),
+    });
+  });
